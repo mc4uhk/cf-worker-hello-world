bbd28ac0ea5a386c48fc2212a73684adec16cd69
Edmund Yau <edmundyau@asl.com.hk>
2025-12-07 20:15:58 +0800

    updated code

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+// src/index.ts
+var index_default = {
+	async fetch(request, env, ctx) {
+		const url = new URL(request.url);
+		switch (url.pathname) {
+			case '/message':
+				return new Response('Hello, World!');
+			case '/env':
+				return new Response(`env: foo:${env.FOO} bar:${env.BAR}`);
+			case '/random':
+				return new Response(crypto.randomUUID());
+			default:
+				return new Response('Not Found', { status: 404 });
+		}
 	},
-} satisfies ExportedHandler<Env>;
+};
+export { index_default as default };
+//# sourceMappingURL=index.js.map
